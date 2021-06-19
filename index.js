6d1c8cd7c4243b36d6025d3a7738fae1539aa53f
Brandon <bjkrynicki@gmail.com>
2021-06-19 15:05:04 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event)
+{
+    let card = []
+    for (let i = 0; i < names.length; i++) {
+        card.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+
+    return card;
+}
+
+function countDown ()
+{
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown)
+        countDown--;
+    }
+    return countDown;
+}
